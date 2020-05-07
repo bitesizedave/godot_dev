413b33d5de5cccae8eaf7e6ab4a3531f5b3b553b
Dave Madden <bitesizedave@gmail.com>
2020-05-06 20:14:04 -0700

    Added 2d stretch mode
    also set zoom to 3

--- grey_box/src/Actors/Camera/GridSnapper.gd
+++ grey_box/src/Actors/Camera/GridSnapper.gd
@@ -2,12 +2,14 @@ extends Position2D
 
 var grid_size: = Vector2()
 var grid_position: = Vector2()
+var zoom_level: = Vector2()
 
 onready var parent = get_parent()
 
 func _ready() -> void:
-#	grid_size = OS.get_screen_size()*$Camera2D.zoom #for fullscreen
-	grid_size = OS.get_real_window_size()*$Camera2D.zoom
+	zoom_level = $Camera2D.zoom
+#	grid_size = OS.get_screen_size()*zoom_level #for fullscreen
+	grid_size = OS.get_real_window_size()*zoom_level
 	set_as_toplevel(true)
 	update_grid_position()
 	
@@ -22,3 +24,4 @@ func update_grid_position():
 		return
 	grid_position = new_grid_position
 	self.position = grid_position * grid_size
+
