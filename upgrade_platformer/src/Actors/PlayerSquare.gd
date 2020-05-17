b7a8ce6ca03bbad8a7e6f1565ca8ff410e9166e0
Dave Madden <bitesizedave@gmail.com>
2020-05-16 21:17:07 -0700

    Added ledge assist for jumping

--- upgrade_platformer/src/Actors/PlayerSquare.gd
+++ upgrade_platformer/src/Actors/PlayerSquare.gd
@@ -9,28 +9,34 @@ export var drop_speed: = 100.0
 export var jump_boost: = 10.0
 export var jump_boost_counter: = 10
 var jbc: = jump_boost_counter
-
-func _on_area_entered(area: Area2D) -> void:
-	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+export var jump_assist_counter: = 5
+var jac: = 0
+var can_jump: = false
+var is_jumping: = false
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
 	die()
 
-func _ready() -> void:
-	print("controllers connected: ",Input.get_connected_joypads())
+#func _ready() -> void:
+#	print("controllers connected: ",Input.get_connected_joypads())
 
 func _physics_process(delta: float) -> void:
 	if is_dropping(): drop()
 	var is_jump_interrupted: = Input.is_action_just_released("jump") && _velocity.y < 0.0
 	var direction: = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, player_speed,is_jump_interrupted)
+	set_jump_boost_counter()
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	set_jump_boost_counter()
+	set_jump_assist_counter()
+	set_can_jump()
+	set_is_jumping()
 	
 
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		-1.0 if Input.is_action_just_pressed("jump") && is_on_floor() && !is_dropping() else 1.0
+		-1.0 if Input.is_action_just_pressed("jump") && can_jump && !is_dropping() else 1.0
 	)
 
 func calculate_move_velocity(
@@ -46,19 +52,14 @@ func calculate_move_velocity(
 		out.x = lerp(out.x, 0.0, friction)
 			
 	out.y += gravity * get_physics_process_delta_time()
-	if direction.y == -1.0:
+	if direction.y == -1.0 && can_jump && !is_jumping:
 		out.y = speed.y * direction.y
 	if is_jump_interrupted:
 		out.y = 0.0
 	if jump_boost_counter > 0 && out.y < 0.0: 
 		out.y -= jump_boost * jbc
-	set_jump_boost_counter()
 	return out
 
-func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
-		var out: = linear_velocity
-		out.y = -impulse
-		return out
 
 func is_dropping() -> bool: 
 	return (Input.get_action_strength("down") && Input.is_action_just_pressed("jump") && is_on_floor())
@@ -68,10 +69,28 @@ func drop() -> void:
 	_velocity.y += drop_speed
 
 func set_jump_boost_counter() -> void:
-	if is_on_floor(): jbc = jump_boost_counter
+	if can_jump: jbc = jump_boost_counter
 	elif jbc > 0 && Input.get_action_strength("jump") > 0.0: 
 		jbc -= 1
-		print("jbc = ",jbc)
+
+func set_jump_assist_counter() -> void:
+	if is_on_floor(): 
+		jac = jump_assist_counter
+	elif jump_assist_counter > 0:
+		jac -= 1
+	else:
+		jac = 0
+
+func set_can_jump() -> void:
+	if jac > 0:
+		can_jump = true
+	else: 
+		can_jump = false
+
+func set_is_jumping() -> void:
+	if _velocity.y < 0.0 && Input.get_action_strength("jump") > 0 : is_jumping = true
+	else: is_jumping = false
+	print(is_jumping)
 
 func die() -> void:
 	WorldData.deaths += 1
