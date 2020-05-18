ff19c9b53c4db927522bb1d17af60285b0079a70
Dave Madden <bitesizedave@gmail.com>
2020-05-18 15:13:40 -0700

    Adjusted gridsnapper to start at mainscene 0,0 transform

--- upgrade_platformer/src/Actors/GridSnapper.gd
+++ upgrade_platformer/src/Actors/GridSnapper.gd
@@ -4,14 +4,15 @@ var grid_size: = Vector2()
 var grid_position: = Vector2() 
 
 onready var parent = get_parent()
+onready var grandparent = parent.get_owner()
 
 func _ready() -> void:
 	grid_size = OS.get_real_window_size()
 	set_as_toplevel(true)
-	update_grid_position()
+#	update_grid_start_position()
 	
 func _physics_process(delta: float) -> void:
-	if !WorldData.get_wrapping(): update_grid_position()
+	if !WorldData.wrapping: update_grid_position()
 
 func update_grid_position():
 	var x = round(parent.position.x/grid_size.x)
@@ -22,6 +23,17 @@ func update_grid_position():
 	grid_position = new_grid_position
 	self.position = grid_position * grid_size
 	WorldData.camera_position = self.position
+	
+func update_grid_start_position():
+	var x = round(grandparent.position.x/grid_size.x)
+	var y = round(grandparent.position.y/grid_size.y)
+	var new_grid_position = Vector2(x,y)
+	if grid_position == new_grid_position:
+		return
+	grid_position = new_grid_position
+	self.position = grid_position * grid_size
+	WorldData.camera_position = self.position
+	
 
 
 
