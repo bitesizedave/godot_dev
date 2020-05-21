9c28d22d50a63c1c4c5bfd0fd657a74dc7f1d932
bitesizedave <bitesizedave@users.noreply.github.com>
2020-05-21 14:15:12 -0700

    StateMachine.gd code added

--- 2d-platformer-course-gdquest/00.start/src/Main/StateMachine/State.gd
+++ 2d-platformer-course-gdquest/00.start/src/Main/StateMachine/State.gd
@@ -11,11 +11,21 @@ Use State as a child of StateMachine Node
 
 onready var _state_machine: = _get_state_machine(self)
 
-func _unhandled_input(event: InputEvent) -> void:
+func unhandled_input(event: InputEvent) -> void:
 	return
+
 
 func physics_process(delta: float) -> void:
 	return
+
+
+func enter(msg: Dictionary = {}) -> void:
+	return
+
+
+func exit() -> void:
+	return
+
 
 func _get_state_machine(node: Node) -> Node:
 	if node != null and not node.is_in_group("state_machine"):
--- 2d-platformer-course-gdquest/00.start/src/Main/StateMachine/StateMachine.gd
+++ 2d-platformer-course-gdquest/00.start/src/Main/StateMachine/StateMachine.gd
@@ -1,2 +1,42 @@
 extends Node
 class_name StateMachine, "res://assets/icons/state_machine.svg"
+"""
+Generic State Machine. Initializes states and delegates engine callbacks
+(_physics_process, _unhandled_input) to the active state.
+"""
+
+export var initial_state: = NodePath()
+
+onready var state: State = $initial_state setget set_state
+onready var _state_name: = state.name
+
+
+func _init():
+	add_to_group("state_machine")
+
+
+func _ready():
+	yield(owner, "ready")
+	state.enter()
+
+
+func _unhandled_input(event: InputEvent):
+	state.unhandled_input(event)
+
+func _physics_process(delta: float):
+	state.set_physics_process(delta)
+
+
+func transition_to(target_state_path: String, msg: Dictionary = {}):
+	if not has_node(target_state_path):
+		return
+	var target_state = $target_state_path
+	state.exit()
+	self.state = target_state
+	state.enter(msg)
+	
+
+func set_state(value: State) -> void:
+	state = value
+	_state_name = state.name
+
