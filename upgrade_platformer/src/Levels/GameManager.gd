9135f4991fea1f35d058d4fc5d7452ba9771fb56
bitesizedave <bitesizedave@users.noreply.github.com>
2020-06-19 09:12:56 -0700

    adjusted snap grid resolution

--- upgrade_platformer/src/Levels/GameManager.gd
+++ upgrade_platformer/src/Levels/GameManager.gd
@@ -6,8 +6,8 @@ onready var coin_scene = preload("res://src/Objects/Coin.tscn")
 func _on_Battling_entered():
 	WorldData.set_wrapping(true)
 	var coin = coin_scene.instance()
-	coin.position.y = 34
-	coin.position.x = 0
+	add_child(coin)
+	coin.position = Vector2(100,50)
 	BattleTimer.start_battle_timer()
 
 func _on_NotBattling_entered():
