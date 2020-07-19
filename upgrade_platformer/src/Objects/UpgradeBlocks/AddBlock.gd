0614bfcf925d41469b019b6fbecdf3326a863450
bitesizedave <bitesizedave@users.noreply.github.com>
2020-07-19 13:25:26 -0700

    Added a generic upgrade block scene and class
    Now to add the animations to it so I don't have to add them individually later

---
+++ upgrade_platformer/src/Objects/UpgradeBlocks/AddBlock.gd
@@ -0,0 +1,90 @@
+extends StaticBody2D
+class_name AddBlock
+
+onready var add_area = $AddArea
+onready var add_area_shape = $AddArea/AddShape
+onready var subtract_area = $SubtractArea
+onready var subtract_area_shape = $SubtractArea/SubtractShape
+onready var level_label = $LevelLabel
+onready var cost_label = $CostLabel
+var points_spent: int
+var starting_cost: = 1
+var cost: float
+var cost_ramp: = 2
+var level: = 1
+var attack_direction: Vector2
+
+func _ready():
+	add_area.connect("area_entered", self, "_on_add_area_entered")
+	add_area.connect("area_exited", self, "_on_add_area_exited")
+	subtract_area.connect("area_entered", self, "_on_subtract_area_entered")
+	subtract_area.connect("area_exited", self, "_on_subtract_area_exited")
+	#load level here
+	if level == 1:
+		cost = starting_cost
+	else: cost *= cost_ramp * level
+	level_label.text = str(level)
+	cost_label.text = str("$",cost)
+
+
+func _on_you_got_thwacked(area, thwack_id, attack_dir):
+	attack_direction = attack_dir
+	if attack_dir.y == 1:
+		add_area_shape.disabled = true
+	else:
+		subtract_area_shape.disabled = true
+
+
+func _on_thwack_timeout():
+	subtract_area_shape.disabled = false
+	add_area_shape.disabled = false
+
+
+
+func _on_subtract_area_entered(area):
+	if area.is_in_group("THWACK"):
+		area.connect("you_got_thwacked", self, "_on_you_got_thwacked")
+		area.timer.connect("timeout", self, "_on_thwack_timeout")
+		if (level > 1
+			and attack_direction.y == 1):
+			WorldData.score += cost
+			points_spent -= cost
+			level -= 1
+			subtract_some_stuff()
+			cost = round(cost/cost_ramp)
+			level_label.text = str(level)
+			cost_label.text = str("$",cost)
+
+
+
+func _on_subtract_area_exited(area):
+	pass
+
+func _on_add_area_entered(area):
+	if (area.is_in_group("THWACK")
+		or area.is_in_group("PLAYER")):
+		if area.is_in_group("THWACK"):
+			area.connect("you_got_thwacked", self, "_on_you_got_thwacked")
+			area.timer.connect("timeout", self, "_on_thwack_timeout")
+		if attack_direction.y != 1:
+			if WorldData.score >= cost:
+				WorldData.score -= cost
+				points_spent += cost
+				level += 1
+				add_some_stuff()
+				cost = round(cost * cost_ramp)
+				level_label.text = str(level)
+				cost_label.text = str("$",cost)
+
+
+
+func _on_add_area_exited(area):
+	pass
+
+# To be overwritten by children to give their specific blocks functionality
+func subtract_some_stuff():
+	pass
+
+# To be overwritten by children to give their specific blocks functionality
+func add_some_stuff():
+	pass
--- upgrade_platformer/src/Objects/UpgradeBlocks/CoinAddBlock.gd
+++ upgrade_platformer/src/Objects/UpgradeBlocks/CoinAddBlock.gd
@@ -1,79 +1,15 @@
-extends StaticBody2D
+extends AddBlock
 
-onready var add_area = $AddArea
-onready var add_area_shape = $AddArea/AddShape
-onready var subtract_area = $SubtractArea
-onready var subtract_area_shape = $SubtractArea/SubtractShape
-onready var level_label = $LevelLabel
-onready var cost_label = $CostLabel
-var points_spent: int
-var starting_cost: = 1
-var cost: float
-var cost_ramp: = 2
-var level: = 1
-var attack_direction: Vector2
 
 func _ready():
-	add_area.connect("area_entered", self, "_on_add_area_entered")
-	add_area.connect("area_exited", self, "_on_add_area_exited")
-	subtract_area.connect("area_entered", self, "_on_subtract_area_entered")
-	subtract_area.connect("area_exited", self, "_on_subtract_area_exited")
-	level = WorldData.start_coins
-	if level == 1:
-		cost = starting_cost
-	else: cost *= cost_ramp * level
-	level_label.text = str(level)
-	cost_label.text = str("$",cost)
+	WorldData.start_coins = level
 
 
-func _on_you_got_thwacked(area, thwack_id, attack_dir):
-	attack_direction = attack_dir
-	if attack_dir.y == 1:
-		add_area_shape.disabled = true
-	else:
-		subtract_area_shape.disabled = true
-
-func _on_subtract_area_entered(area):
-	if area.is_in_group("THWACK"):
-		area.connect("you_got_thwacked", self, "_on_you_got_thwacked")
-		area.timer.connect("timeout", self, "_on_thwack_timeout")
-		if (level > 1
-			and attack_direction.y == 1):
-			WorldData.score += cost
-			points_spent -= cost
-			level -= 1
-			WorldData.set_start_coins(level)
-			cost = round(cost/cost_ramp)
-			level_label.text = str(level)
-			cost_label.text = str("$",cost)
+# To be overwritten by children to give their specific blocks functionality
+func subtract_some_stuff():
+	WorldData.start_coins -= 1
 
 
-
-func _on_subtract_area_exited(area):
-	pass
-
-func _on_add_area_entered(area):
-	if (area.is_in_group("THWACK")
-		or area.is_in_group("PLAYER")):
-		if area.is_in_group("THWACK"):
-			area.connect("you_got_thwacked", self, "_on_you_got_thwacked")
-			area.timer.connect("timeout", self, "_on_thwack_timeout")
-		if attack_direction.y != 1:
-			if WorldData.score >= cost:
-				WorldData.score -= cost
-				points_spent += cost
-				level += 1
-				WorldData.set_start_coins(level)
-				cost = round(cost * cost_ramp)
-				level_label.text = str(level)
-				cost_label.text = str("$",cost)
-
-
-
-func _on_add_area_exited(area):
-	pass
-
-
-func _on_thwack_timeout():
-	subtract_area_shape.disabled = false
-	add_area_shape.disabled = false
+# To be overwritten by children to give their specific blocks functionality
+func add_some_stuff():
+	WorldData.start_coins += 1
