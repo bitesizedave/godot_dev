852db6ce0bb65d962dcbb2cec11957b30818d64b
bitesizedave <bitesizedave@users.noreply.github.com>
2020-08-07 12:35:21 -0700

    Adjusted PlayerTrail2D to only emit when dashing

--- upgrade_platformer/src/Actors/Player/Player.gd
+++ upgrade_platformer/src/Actors/Player/Player.gd
@@ -2,7 +2,7 @@ extends Actor
 class_name Player
 
 onready var state_machine: StateMachine = $StateMachine
-#onready var trail: Trail2D = $PlayerTrail2D
+onready var trail: Trail2D = $PlayerTrail2D
 onready var collider: CollisionShape2D = $CollisionShape2D
 onready var shaking_camera: Camera2D = $CameraRig/ShakingCamera
 
@@ -12,7 +12,7 @@ const PASSTHROUGH_BIT_LAYER: = 3
 
 func _ready():
 	WorldData.connect("instant_battle_started", self, "_on_instant_battle_started")
-#	trail.set_is_emitting(true)
+	trail.set_emitting(false)
 
 func set_is_active(value: bool) -> void:
 	is_active = value
@@ -23,11 +23,14 @@ func set_is_active(value: bool) -> void:
 
 func _on_dash_started():
 	set_collision_mask_bit(PASSTHROUGH_BIT_LAYER, false)
+	trail.set_emitting(true)
 
 
 func _on_dash_ended():
 	set_collision_mask_bit(PASSTHROUGH_BIT_LAYER, true)
-
+	#need to yield a bit because the "end" of the dash is technically Move State
+	yield(get_tree().create_timer(0.08), "timeout")
+	trail.set_emitting(false)
 
 func _on_instant_battle_started():
 	position = PlayerData.player_game_start_position
